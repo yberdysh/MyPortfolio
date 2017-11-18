e46efb6f9be798a7767fb76eeacca931ad2b22d4
Jane Costa <yberdysh@gmail.com>
2017-11-18 18:17:11 -0500

    trying to seed pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Project.destroy_all
 
 Project.create(
   name: "TaskIt!",
@@ -14,5 +14,6 @@ Project.create(
   The innovative product that my team and I created is TaskIt. This is a platform for both employers looking to hire and students or young adults looking to apply for internships.
   With TaskIt, employers of startups and small companies can post their company's internship positions in order to hire someone on a short-term basis of just a few weeks.
   Students and young adults who crave experience in a new field can create a profile similar to LinkedIn, browse internships, and apply easily by sending their epplicaiton straight to the
-  employer's inbox."
+  employer's inbox.",
+  photo: open("../../app/assets/images/taskit.png")
   )
