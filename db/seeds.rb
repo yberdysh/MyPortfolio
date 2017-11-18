5886773c7a56444da3effa75e7a3ce3701290279
Jane Costa <yberdysh@gmail.com>
2017-11-18 18:07:32 -0500

    added about section and projects section

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Project.create(
+  name: "TaskIt!",
+  link: "https://alexolthofftaskit.herokuapp.com/",
+  description: "I worked in a team of three to take on the challenge of coding a marketplace website in only five days.
+  The innovative product that my team and I created is TaskIt. This is a platform for both employers looking to hire and students or young adults looking to apply for internships.
+  With TaskIt, employers of startups and small companies can post their company's internship positions in order to hire someone on a short-term basis of just a few weeks.
+  Students and young adults who crave experience in a new field can create a profile similar to LinkedIn, browse internships, and apply easily by sending their epplicaiton straight to the
+  employer's inbox."
+  )
